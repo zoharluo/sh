56c02fe20204a02ff2ef5217bb4d9e598c8bf380
zoharluo <zohar_luo@hotmail.com>
2024-12-04 05:38:11 +0800

    Update zohar.sh

--- zohar.sh
+++ zohar.sh
@@ -68,15 +68,12 @@ add_alias(){
     echo "$ALIAS_Z" >> ~/.bashrc
     echo "$ALIAS_ZOHAR" >> ~/.bashrc
     echo "z,zohar 添加到快速启动脚本。"
+    # 立即在当前 shell 中创建别名
+    alias z="$SCRIPT_PATH"
+    alias zohar="$SCRIPT_PATH"
   fi
-  
-  # 立即在当前 shell 中创建别名
-  alias z="$SCRIPT_PATH"
-  alias zohar="$SCRIPT_PATH"
 }
 
-# 确保脚本有执行权限
-chmod +x "$0"
 
 # 添加别名
 add_alias
